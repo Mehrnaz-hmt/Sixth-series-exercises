f945509c9a5f51e2f660f20137783b63c6222da0
Mehrnaz <mehrnazhemmat1998@gmail.com>
2021-12-15 13:47:32 +0330

    second question added

--- Question 2/question-2.js
+++ Question 2/question-2.js
@@ -1,10 +1,18 @@
-let a = [-1, 150, 190, 170, -1, -1, 160, 180];
+let arr = [-1, 150, 190, 170, -1, -1, 160, 180];
 let b = [-1, 150, 160, 170, -1, -1, 180, 190]
 
-function sortTallPeople (arr) {
-    if (Array.isArray(arr)) {
-        let sortedArray = arr.sort()
-        console.log(sortedArray)
-    }
-} 
-console.log(sortTallPeople(a))
+function sortByHeight(a) {
+
+const sortedA = [...a].sort((a, b) => a - b).filter((num) => num !== -1);
+let sortedAIndex = 0;
+for (let i = 0; i < a.length; i++) {
+
+    if (a[i] === -1) continue;
+    else {
+
+a[i] = sortedA[sortedAIndex++];
+}
+}
+return a;
+}
+console.log(sortByHeight(arr))
